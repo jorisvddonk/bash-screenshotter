01da8657adb2e4a1565758ad42218e849261396c
joris <jorisvddonk@gmail.com>
2018-05-23 19:14:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bash.org quote screenshotter
+
+Makes screenshots of the top quotes on bash.org, via [Puppeteer](https://github.com/GoogleChrome/puppeteer/)
+
+## Usage
+
+1. `npm install`
+2. `node index.js`
---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://bash.org/?top");
+
+  /* Since puppeteer can only screenshot a single element and its children, and since Bash.org splits up entire quotes in two,
+     we have to modify DOM such that every quote has a parent element we can then screenshot later on. */
+  await page.evaluate(function() {
+    var newQuotes = document.createElement("div");
+    newQuotes.setAttribute("id", "newQuotes");
+    document.querySelectorAll("center table td")[4].appendChild(newQuotes);
+    var quotes = document.querySelectorAll(".quote");
+    for (let quote of quotes) {
+      var newquote = document.createElement("div");
+      newquote.setAttribute("class", "newquote");
+      var sib = quote.nextSibling;
+      quote.remove();
+      sib.remove();
+      newquote.appendChild(quote);
+      newquote.appendChild(sib);
+      newQuotes.appendChild(newquote);
+    }
+  });
+
+  /* Make screenshots of all quotes. Puppeteer can't screenshot elements in parallel, so we have to do this sequentially.
+     Luckily, with `await` that's easy :D */
+  const elements = await page.$$(".newquote");
+  for (var i = 0; i < elements.length; i++) {
+    await elements[i].screenshot({ path: `quote_${i}.png` });
+  }
+  await browser.close();
+})();
